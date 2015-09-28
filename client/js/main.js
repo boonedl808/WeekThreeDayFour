420d22f53f0c5379e522f70852116f3c8909ca2d
Daniel Boone <DanielBoone@Daniels-MacBook-Pro.local>
2015-09-28 08:56:28 -0500

    Push 3

--- client/js/main.js
+++ client/js/main.js
@@ -1,29 +1,72 @@
-$(document).ready(function() {
-/*Using document ready runs code only after the DOM is ready for js code to run more on that here: https://learn.jquery.com/using-jquery-core/document-ready */
-		/*This function should create a post request using jquery. When posted it should:
-			1) Add tweets to the 'database'
-			2) After posted prepend message to list of messages and clear input box */
-	
-	function postData() {
-		$("button").click(function(){
-    	$.post("text box",
-    {
-        username: "default"
-    },
-    function(data, status){
-        console.log("Data: " + data + "\nStatus: " + status);
-    });
-});
-	}
+$(document).ready(function(){
 
-	function getData() {
-		/*This function should make a get request from 'database', parse the data and prepend each to the page*/
-		$("button").click(function() {
-    		$.get("messages", function(data, status) {
-        console.log("Data: " + data + "\nStatus: " + status);
-    	});
-	});
-}
-	/*Calls function once page loaded to display tweets to page*/
-	getData();
+    var tweets = [];
+
+    $('.tweet-button').click(function(){
+        if(!validateTweet()) {
+            alert('Missing data!');
+            return;
+        }
+        
+        var tweetText = $('.tweet-text').val();
+        postData(tweetText, $('.tweet-user').val());
+    });
+
+    function postData(text, userName) {
+        var tweet = {};
+        tweet.text = text;
+        tweet.userName = userName;
+        $.ajax({
+            url: '/messages',
+            method: 'POST',
+            data: JSON.stringify(tweet)
+        }).done(function(result) {
+            addTweet(tweet);
+            clearTweet();
+        }).fail(function(err) {
+            alert(err);
+        });
+    }
+
+    function getData() {
+        $.ajax({
+            url: '/messages'
+        }).done(function(results){
+            var t = results.split('\n');
+            var totTweets = tweets.length == 0 ? 0 : tweets.length - 1;
+            for (var i = totTweets; i < t.length; i++) {
+                var tweet = JSON.parse(t[i]);
+                tweets.push(tweet);
+                addTweet(tweet);
+            }
+        });
+    }
+    
+    function addTweet(tweet) {
+        var divTweets = $('.tweets');
+        divTweets.prepend(createTweetDiv(tweet));
+    }
+
+    function createTweetDiv(tweet){
+        var tdiv = $('<div class="tweet col-md-12"></div>');
+        tdiv.text(tweet.userName + ': ' + tweet.text);
+        return tdiv;
+    }
+
+    function validateTweet() {
+        if($('.tweet-text').val() === '' || 
+            $('.tweet-user').val() === '') {
+                return false;
+            }
+        return true;
+    }
+
+    function clearTweet() {
+        $('.tweet-text').val('');
+    }
+
+    setInterval(
+        getData, 
+        1000
+    );
 });
